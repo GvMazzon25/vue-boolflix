13df29d2c1609cefbe48cf81e6491c6bfea8948a
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-29 17:52:14 +0100

    result.data.result

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -35,8 +35,8 @@ export default {
         axios
              .get('https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro')
              .then(result =>{
-               console.log(result.data.result);
-               this.cardList = result.data.result;
+               console.log(result.data);
+               this.cardList = result.data.result
              })
              .catch(err => console.log(err));
       }
