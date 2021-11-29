9086d15208e1c425c8cdb89db01eba11f9807d5b
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-29 13:19:10 +0100

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+      <h2>Boolflix</h2>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+   @import '@/style/header.scss';
+</style>
