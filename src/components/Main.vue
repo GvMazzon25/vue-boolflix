efa82e33308b718035bc07354316fe124964a724
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-29 15:59:30 +0100

    start-build

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,38 @@
 <template>
   <main>
-
+      <ul>
+          <li><h4>Titolo:</h4></li>
+          <li>Titolo Originale:</li>
+          <li>Lingua</li>
+          <li>Voto</li>
+      </ul>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    data() {
+      return {
+        cardList: null,
+      }
+    },
+    created() {
+      this.getCards();
+    },
+    methods: {
+      getCards() {
+        axios
+             .get('https://api.themoviedb.org/3/movie/550?api_key=e9578c42c322fd364bb6266a886f6239&query=Fight+Club&language=in-IT')
+             .then(result =>{
+               console.log(result.data);
+               this.cardList = result.data
+             })
+             .catch(err => console.log(err));
+      }
+    }
 }
 </script>
 
